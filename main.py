f04abc59c57a5442c9e583c74dec19b886095693
Priyanka Bharti <priyankabharti@PriyankasLaptop.fritz.box>
2023-12-27 10:32:53 +0100

    added data ingestion pipeline

--- main.py
+++ main.py
@@ -1,5 +1,16 @@
+from textSummarizer.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 from textSummarizer.logging import logger
 
-logger.info("Welcome!")
+STAGE_NAME = "Data Ingestion Stage"
+
+STAGE_NAME = "Data Ingestion stage"
+try:
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
+   data_ingestion = DataIngestionTrainingPipeline()
+   data_ingestion.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
 
 #python /Users/priyankabharti/Documents/Projects/Text-Summarization/main.py
